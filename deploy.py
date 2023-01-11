ba62c1f424f63a44e5c9b12830242b04fac0578a
Cloufield <610935659@qq.com>
2023-01-11 16:00:33 +0900

    updated deploy.py and mkdocs

--- deploy.py
+++ deploy.py
@@ -9,25 +9,58 @@ repo_url: https://github.com/Cloufield/CTGCatalog/
 edit_uri: ""
 copyright: "CTGCatalog is licensed under the MIT license"
 theme:
-  name: null
-  custom_dir: 'cinder'
-  color_theme:  github"
+  name: material
+  features:
+    - navigation.tabs
+  font:
+    code: Roboto Mono
+    text: Roboto
+  palette:
+    primary: indigo
+    accent: indigo
 extra_css:
   - "stylesheets/extra.css"
+markdown_extensions:
+  - toc:
+      toc_depth: 3
+  - admonition
+  - pymdownx.details
+  - pymdownx.superfences
+  - pymdownx.arithmatex:
+      generic: true
+
+extra_javascript:
+  - javascripts/mathjax.js
+  - https://polyfill.io/v3/polyfill.min.js?features=es6
+  - https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js
+
+plugins:
+  - mkdocs-jupyter
 '''
 shutil.copyfile("./README.md", "./docs/index.md")
 part2="nav: \n    - Home : index.md\n"
 for dirname in ["Tools","Sumstats","Reference_data","Visualization"]: #"Programming","NGS",
     for root, dirs, files in os.walk("./"+dirname):
         #print(root)
-        currentfolder=root.strip(".|/").split("/")[-1]
+	
+	## get current directory
+        currentfolder = root.strip(".|/").split("/")[-1]
         files.sort()
+	
+	## for files in the current directory
         for name in files:
+	    ## get full path for a file
             file = os.path.join(root,name)
+	    
+            ## reconstruct the name / in path to _
             filerename = ("_".join(file.strip(".|/").split("/")))
-            if len(dirs)>0:
+            
+            ## if there are subdirectories
+            if filerename[-2:]=="md" and len(dirs)>0:
+                ## file sub categories
                 part2+=" "*2*len(file.strip(".|/").split("/"))+"- " + root.strip(".|/").split("/")[-1] +":"
                 part2+="\n"
+                ## file md
                 part2+=" "*2*(1+len(file.strip(".|/").split("/")))+"- " + root.strip(".|/").split("/")[-1] +": "+filerename
                 part2+="\n"
                 shutil.copyfile(file, "./docs/"+filerename)
